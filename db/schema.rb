3879288e38851834be50e7a3a948915d01a5e98d
Jason Vang <jvang@Jasons-MacBook-Air.local>
2024-01-16 10:58:13 -0600

    Added role to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_16_024952) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_16_165414) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -46,10 +46,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_16_024952) do
     t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "role_id"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["role_id"], name: "index_users_on_role_id"
   end
 
   add_foreign_key "children", "users"
+  add_foreign_key "users", "roles"
 end
