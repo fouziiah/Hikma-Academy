c7269e658dee15b4591a4b733b8763c965a6a5da
Jason Vang <jvang@Jasons-MacBook-Air.local>
2024-01-15 20:52:11 -0600

    Added Child model and controller actions

---
+++ db/migrate/20240116024952_create_children.rb
@@ -0,0 +1,12 @@
+class CreateChildren < ActiveRecord::Migration[7.1]
+  def change
+    create_table :children do |t|
+      t.string :first_name
+      t.string :last_name
+      t.date :date_of_birth
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
