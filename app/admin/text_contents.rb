9d164bdb9646554c0c5993b259a356e74914fa04
fouziah ahmed <fouziahahmed@fouziahs-MacBook-Air.local>
2024-02-08 22:08:10 -0600

    update the teachers dashboard

--- app/admin/text_contents.rb
+++ app/admin/text_contents.rb
@@ -2,4 +2,25 @@
 
 ActiveAdmin.register TextContent do
   permit_params :title, :body, :location
+
+  index do
+    selectable_column
+    id_column
+    column :title
+    column 'Body' do |text_content|
+      shortened_body = text_content.body.truncate(50, separator: ' ', omission: '...')
+      span shortened_body, title: text_content.body
+    end
+    column :created_at
+    column :updated_at
+    actions
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :title
+      f.input :body
+    end
+    f.actions
+  end
 end
