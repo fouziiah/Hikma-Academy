8d4941329c5cc8dc41f01af3587f8903877a9680
fouziah ahmed <fouziahahmed@fouziahs-MacBook-Air.local>
2024-02-13 01:09:42 -0600

    removed email confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,8 +13,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :confirmable
+         :recoverable, :rememberable, :validatable
+         #:confirmable
 
   pay_customer stripe_attributes: :stripe_attributes
 
